51abc12e9ca1d16a572db41398a6235cd3d8b720
Narwin A <narwinatienza@gmail.com>
2024-04-03 22:08:27 +0800

    Add database connection configuration

--- api/server.js
+++ api/server.js
@@ -1,11 +1,28 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+// database connection
+const connect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+  connect();
+  console.log(`Server is running on http://localhost:${port}`);
 });
