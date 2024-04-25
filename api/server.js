42b782d8f0b74089fda32c54a851bffb1389a95e
Narwin A. <narwinatienza@gmail.com>
2024-04-25 22:19:49 +0800

    Create verify user middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/userRoute.js";
 dotenv.config();
@@ -21,6 +22,8 @@ const connect = async () => {
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
