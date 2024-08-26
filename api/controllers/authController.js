c11dca6a7090a1e1600a43f84abed261cfdd860f
Narwin A. <narwinatienza@gmail.com>
2024-08-26 22:48:41 +0800

    chore(model, controller): add new fields in user model and update auth controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,17 +1,15 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
-  const { email, password } = req.body;
-
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ username, email });
     if (user) return next(errorHandler(400, "User already exists"));
 
-    const hash = bcrypt.hashSync(password, 10);
-    const newUser = new User({ email, password: hash });
+    const hash = bcryptjs.hashSync(req.body.password, 10);
+    const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
 
@@ -25,16 +23,16 @@ export const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email });
-    if (!user) return next(errorHandler(404, "User not found"));
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return next(errorHandler(401, "Invalid credentials"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
       expiresIn: "30d",
     });
-    const { password: pass, ...info } = user._doc;
+    const { password: pass, ...info } = validUser._doc;
 
     res.cookie("accessToken", token, { httpOnly: true }).status(200).json(info);
   } catch (err) {
