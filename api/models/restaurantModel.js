47ebd32b2fbc4be58a40d9d25a3546f96604d25b
Narwin A. <narwinatienza@gmail.com>
2024-08-27 23:19:05 +0800

    feat: add Restaurant model

---
+++ api/models/restaurantModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  menuName: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const restaurantSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  restaurantName: { type: String, required: true },
+  location: { type: String, required: true },
+  menus: [menuSchema],
+  images: { type: String, required: true },
+  lastUpdated: { type: Date, required: true },
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+export default Restaurant;
